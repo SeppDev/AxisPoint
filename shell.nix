a644c7ad4b404a96e75d9a1036eef431d50b0d31
frittesauce <181068943+frittesauce@users.noreply.github.com>
2025-09-10 12:02:22 +0200

    we on rails now

---
+++ shell.nix
@@ -0,0 +1,22 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+pkgs.mkShell {
+  buildInputs = [
+    pkgs.ruby
+    pkgs.nodejs
+    pkgs.yarn
+    pkgs.sqlite
+    pkgs.libyaml   # ✅ Required for psych
+    pkgs.gnumake
+    pkgs.gcc
+    pkgs.pkg-config
+    pkgs.zlib
+    pkgs.openssl
+    pkgs.readline
+  ];
+
+  shellHook = ''
+    export GEM_HOME=$HOME/.gem
+    export PATH=$GEM_HOME/bin:$PATH
+  '';
+}
