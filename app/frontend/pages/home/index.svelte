0da30a80979500b21dc8a0beb47f915de9aa504e
frittesauce <181068943+frittesauce@users.noreply.github.com>
2025-10-02 11:57:38 +0200

    added dynamically themese based on browser prefrecnees

--- app/frontend/pages/home/index.svelte
+++ app/frontend/pages/home/index.svelte
@@ -7,8 +7,8 @@
     <div class="flex flex-row gap-x-12 justify-center items-center">
         <img src="https://media1.tenor.com/m/nj8AyQWPJ-UAAAAd/h2di-cat-dead.gif" alt="temp" class=" h-[400px] w-[400px] rounded-xl">
         <div class="flex flex-col">
-            <h1 class=" text-5xl text-ctp-base font-bold">AxisPoint!</h1>
-            <p class="text-xl text-ctp-base " >de beste website om dingen te fixxen</p>
+            <h1 class=" text-5xl font-bold">AxisPoint!</h1>
+            <p class="text-xl  " >de beste website om dingen te fixxen</p>
         </div>
     </div>
 </div>
