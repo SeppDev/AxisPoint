7440a59462ca6e53ef77e09053d82d600b5138c7
SeppDev <seppdev@noreply.codeberg.org>
2025-10-10 10:22:57 +0200

    Klachten DB migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_08_01_153828) do
+ActiveRecord::Schema[8.0].define(version: 2025_10_10_081330) do
+  create_table "klachten", force: :cascade do |t|
+    t.float "longitude", null: false
+    t.float "latitude", null: false
+    t.string "title", null: false
+    t.text "description", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "sessions", force: :cascade do |t|
     t.integer "user_id", null: false
     t.string "user_agent"
