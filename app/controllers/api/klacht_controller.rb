8023255e0673fa790f28aed1b7ea4830152d7635
frittesauce <181068943+frittesauce@users.noreply.github.com>
2025-11-03 11:34:51 +0100

    added image support!

--- app/controllers/api/klacht_controller.rb
+++ app/controllers/api/klacht_controller.rb
@@ -1,19 +1,20 @@
 class Api::KlachtController < ApplicationController
-
   before_action :authenticate, only: [:index]
-  
+
   # Skip authentication for :create (POST)
   skip_before_action :authenticate, only: [:create]
 
-  protect_from_forgery with: :null_session 
-  
+  protect_from_forgery with: :null_session
+
   def create
-    klacht = Klacht.new(body_params)
+    klacht = Klacht.new(klacht_params)
+
+    klacht.image.attach(params[:klacht][:image]) if params[:klacht][:image].present?
 
     if klacht.save
-      render json: { status: "success", item: body_params}
-    else 
-      render json: { errors: post.errors.full_messages }, status: :unprocessable_entity
+      render json: klacht, status: :created
+    else
+      render json: { errors: klacht.errors.full_messages }, status: :unprocessable_entity
     end
   end
 
@@ -24,7 +25,7 @@ class Api::KlachtController < ApplicationController
 
   private
 
-  def body_params
-    params.require(:klacht).permit(:name, :description, :latitude, :longitude)
-  end 
+  def klacht_params
+    params.require(:klacht).permit(:name, :description, :latitude, :longitude, :image)
+  end
 end
